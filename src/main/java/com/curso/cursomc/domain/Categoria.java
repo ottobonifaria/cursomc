8ea3014d4df142ed89acb26e230bd4e12b12d964
ottobonifaria <ottobonifaria.live.com>
2021-07-13 10:18:39 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/curso/cursomc/domain/Categoria.java
+++ src/main/java/com/curso/cursomc/domain/Categoria.java
@@ -1,11 +1,10 @@
 package com.curso.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Categoria implements Serializable{
@@ -15,7 +14,10 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
+
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
+
 	public Categoria() {
 		
 	}
@@ -40,6 +42,14 @@ public class Categoria implements Serializable{
 
 	public void setNomeString(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
---
+++ src/main/java/com/curso/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.curso.cursomc.repositories;
+
+import com.curso.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository <Produto,Integer>{
+
+}
