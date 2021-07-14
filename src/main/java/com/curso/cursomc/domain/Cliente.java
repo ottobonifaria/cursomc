4814b8f8de0db086e6095ebef8aa09868ca2b565
ottobonifaria <ottobonifaria.live.com>
2021-07-14 12:27:23 -0300

    EstadoPagamentp e Pagamento

--- src/main/java/com/curso/cursomc/domain/Cliente.java
+++ src/main/java/com/curso/cursomc/domain/Cliente.java
@@ -27,6 +27,9 @@ public class Cliente implements Serializable {
     @CollectionTable(name = "TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos = new ArrayList<>();
+
     public Cliente() {
     }
 
@@ -70,9 +73,7 @@ public class Cliente implements Serializable {
         this.cpfoucnpj = cpfoucnpj;
     }
 
-    public TipoCliente getTipo() {
-        return TipoCliente.toEnum(tipo);
-    }
+    public TipoCliente getTipo() { return TipoCliente.toEnum(tipo); }
 
     public void setTipo(TipoCliente tipo) {
         this.tipo = tipo.getCod();
@@ -93,6 +94,10 @@ public class Cliente implements Serializable {
     public void setTelefones(Set<String> telefones) {
         this.telefones = telefones;
     }
+
+    public List<Pedido> getPedidos() {return pedidos;}
+
+    public void setPedidos(List<Pedido> pedidos) { this.pedidos = pedidos; }
 
     @Override
     public boolean equals(Object o) {
