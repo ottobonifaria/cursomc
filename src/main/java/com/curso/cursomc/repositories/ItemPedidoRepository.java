edad0ec27d59ba4a41e04877cd854546b1b2606d
ottobonifaria <ottobonifaria.live.com>
2021-07-14 13:33:30 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/curso/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.curso.cursomc.repositories;
+
+import com.curso.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository <ItemPedido,Integer>{
+
+}
